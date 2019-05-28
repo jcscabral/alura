f3e1e60a443dc300ef37db7c9353f8c9e8a3362d
jcscabral <jcscabral@hotmail.com>
2019-05-27 23:08:35 -0300

    adicionando código inicial

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,83 @@
+class NegociacaoController {
+    
+    constructor() {
+        
+        let $ = document.querySelector.bind(document);
+        
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+         
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia');
+       
+        this._mensagem = new Bind(
+            new Mensagem(), new MensagemView($('#mensagemView')),
+            'texto');       
+    }
+    
+    adiciona(event) {
+        
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
+        this._limpaFormulario();   
+    }
+    
+    importaNegociacoes(){
+
+        let xhr = new XMLHttpRequest();
+        
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.send();
+        /*
+            0
+            1
+            2
+            3
+            4
+        */
+        xhr.onreadystatechange = () =>{
+            if(xhr.readyState ==4 ){
+                if(xhr.status == 200){
+                    console.log('achei');                    
+                    JSON.parse(xhr.responseText)
+                        .map(objeto=> new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                        this._mensagem = "importação realizada com sucesso!";
+                    }
+                else{                    
+                    console.log(xhr.responseText)
+                    this._mensagem = "não achei";
+                }
+                
+                
+            }
+        }
+
+    }
+    
+    apaga() {
+        
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+    }
+    
+    _criaNegociacao() {
+        
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);    
+    }
+    
+    _limpaFormulario() {
+     
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();   
+    }
+}
