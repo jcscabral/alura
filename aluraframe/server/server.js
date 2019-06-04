16a8edbe8ad196096e239b0e3d20abfde9a9caab
jcscabral <jcscabral@hotmail.com>
2019-06-03 21:23:03 -0300

    observação para o Express

--- aluraframe/server/server.js
+++ aluraframe/server/server.js
@@ -1,6 +1,9 @@
 var http = require('http')
     ,app = require('./config/express');
 
+/*
+    TODO: Incrementar servidor
+*/
 http.createServer(app).listen(3000, function() {
     console.log('Servidor estutando na porta: ' + this.address().port);
 });
